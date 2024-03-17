5e95027ddf3efb940007274f66528720de3276fb
Dmytrii Semenikhin <d.semenikhin27@gmail.com>
2024-03-17 17:37:11 +0200

    Added csv save

--- HW3/generate_students.py
+++ HW3/generate_students.py
@@ -1,3 +1,4 @@
+import csv
 from flask import Flask, request
 from faker import Faker
 
@@ -7,7 +8,8 @@ fake = Faker()
 @app.route('/generate_students')
 def generate_students():
     count = request.args.get('count', default=1, type=int)
-    count = min(count, 1000)
+    if count > 1000:
+        return "Error: No more than 1000 students!"
     students = []
     for _ in range(count):
         student = {
@@ -18,7 +20,10 @@ def generate_students():
             'birthday': fake.date_of_birth(minimum_age=18, maximum_age=60)
         }
         students.append(student)
-
+    with open('students.csv', 'w',newline='') as f:
+        writer = csv.DictWriter(f, fieldnames=students[0].keys())
+        writer.writeheader()
+        writer.writerows(students)
     # formatting students for output, testing HTML
     format_students = ""
     for student in students:
