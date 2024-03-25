18a9d93275b164a0f6618ec55d632bdf9168e75a
Dmytrii Semenikhin <d.semenikhin27@gmail.com>
2024-03-25 14:32:36 +0200

    HW5

--- HW4/hw4.py
+++ HW4/hw4.py
@@ -72,3 +72,21 @@ def album_listening_time():
     """
     result = execute_query(query=query)
     return format_records(result)
+
+@app.route("/stats_by_city")
+def stats_by_city():
+    genre = request.args.get('genre', type=str)
+    if not genre:
+        return "Please indicate genre!"
+    query = """
+    SELECT BillingCity FROM invoices
+    JOIN invoice_items ON invoices.InvoiceId = invoice_items.InvoiceId
+    JOIN tracks ON invoice_items.TrackId = tracks.TrackId
+    JOIN genres ON tracks.GenreId = genres.GenreId
+    WHERE genres.Name = ?
+    GROUP BY BillingCity
+    ORDER BY COUNT(*) DESC
+    LIMIT 1
+    """
+    result = execute_query(query=query, args=(genre,))
+    return format_records(result)
